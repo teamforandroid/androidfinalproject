36d1d1a3beef99e4d2f168757f8722351a298bb1
catherine <zhangjiecatherine@gmail.com>
2019-10-26 22:16:44 -0400

    Second update with current location

--- app/src/main/java/android/example/finalproject/Charger.java
+++ app/src/main/java/android/example/finalproject/Charger.java
@@ -1,9 +1,19 @@
 package android.example.finalproject;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 import androidx.fragment.app.FragmentActivity;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.os.Build;
 import android.os.Bundle;
+import android.widget.Toast;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -16,6 +26,10 @@ public class Charger extends FragmentActivity implements OnMapReadyCallback {
 
     GoogleMap map;
 
+    private LocationManager locationManager;
+    private LocationListener locationListener;
+    private Object Bitmap;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -25,7 +39,53 @@ public class Charger extends FragmentActivity implements OnMapReadyCallback {
                 .findFragmentById(R.id.map);
 
         mapFragment.getMapAsync(this);
+
+        locationManager = (LocationManager) this.getSystemService(LOCATION_SERVICE);
+
+        locationListener = new LocationListener() {
+            @Override
+            public void onLocationChanged(Location location) {
+                 //Toast.makeText(Charger.this, location.toString(), Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onStatusChanged(String s, int i, Bundle bundle) {
+
+            }
+
+            @Override
+            public void onProviderEnabled(String s) {
+
+            }
+
+            @Override
+            public void onProviderDisabled(String s) {
+
+            }
+        };
+
+        if (Build.VERSION.SDK_INT < 23) {
+            if (checkSelfPermission(Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED ) {
+                // TODO: Consider calling
+                //    Activity#requestPermissions
+                // here to request the missing permissions, and then overriding
+                //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+                //                                          int[] grantResults)
+                // to handle the case where the user grants the permission. See the documentation
+                // for Activity#requestPermissions for more details.
+                return;
+            }
+            locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, locationListener);
+        }else{
+            if(ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED){
+                ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, 1);
+            }else{
+                locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER,0,0, locationListener);
+            }
+        }
+
     }
+
 
 
     @Override
@@ -33,9 +93,26 @@ public class Charger extends FragmentActivity implements OnMapReadyCallback {
 
         map = googleMap;
 
-        LatLng Ottawa = new LatLng(75.6972, 45.4215);
+        LatLng Ottawa = new LatLng(45.4215, -75.6972);
         map.addMarker(new MarkerOptions().position(Ottawa).title("Ottawa"));
         map.moveCamera(CameraUpdateFactory.newLatLng(Ottawa));
+        //Toast.makeText(this, map.toString(), Toast.LENGTH_SHORT).show();
 
     }
+
+
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if(requestCode == 1){
+            if(grantResults.length > 0 && grantResults[0]== PackageManager.PERMISSION_GRANTED) {
+                if (checkSelfPermission(Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED ){
+                    locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, locationListener);
+                }
+            }
+
+        }
+    }
+
 }
