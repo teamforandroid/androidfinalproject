29ff9401b6914c64f4f902fc49f57bc0b5676b1d
lizy3143 <li000546@aglonquinlive.com>
2019-11-25 15:10:17 -0500

    recipe 11/25

--- app/src/main/java/android/example/finalproject/RecipeDetailActivity.java
+++ app/src/main/java/android/example/finalproject/RecipeDetailActivity.java
@@ -6,14 +6,18 @@ import android.content.ContentValues;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.os.Message;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 
 import java.io.IOException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
 import java.net.URL;
 
 public class RecipeDetailActivity extends AppCompatActivity {
@@ -23,6 +27,7 @@ public class RecipeDetailActivity extends AppCompatActivity {
     private Bitmap image;
     TextView titleTextView;
     ImageView imageView;
+    TextView urlText;
     private Button goSavedButton;
  Button saveRecipe;
 
@@ -30,57 +35,23 @@ public class RecipeDetailActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_recipe_detail);
+//bind  title image url
+        titleTextView=findViewById(R.id.recipeTitleValue);
+        imageView=findViewById(R.id.recipeImage);
+        urlText=findViewById(R.id.recipeLink);
+//get value of title  url image
+        title= getIntent().getStringExtra("title");
+        titleTextView.setText(title);
+        url=getIntent().getStringExtra("url");
 
-        initView();
 
-//insert  setonclicklistener for goto next page
+//get image---can not get image
+//        byte[] byteArray =getIntent().getByteArrayExtra("image");
+//        image = BitmapFactory.decodeByteArray(byteArray, 0, byteArray.length);
+//        imageView.setImageBitmap(image);
 
-   //     goSavedButton=finndViewById(R.id.savedmyfavourite);
-    //    if (goSavedButton != null) {
-    //        goSavedButton.setOnClickListener(v -> {
-    //            Intent goToPage3 = new Intent(RecipeDetailActivity.this, SaveFavoriteActivity.class);
-     //           startActivity(goToPage3);
-     //       });
-    }
-//To bind title image url
-    private void initView(){
 
-         Intent intent = getIntent();
-         title=intent.getStringExtra("title");
-         url=intent.getStringExtra("url");
-         String imageUrl=intent.getStringExtra("imageUrl");
-         try {
-       ////load image
-            URL u = new URL(imageUrl);
-            image = BitmapFactory.decodeStream(u.openConnection().getInputStream());
-        } catch(IOException e) {
-            e.printStackTrace();
-        }
     }
 
-    /// link to recipe item by url to web
-
-///
-
-
-    ////save faveriat
-  //   saveRecipe.setOnClickListener(click -> {
-        TextView titlefave = findViewById(R.id.title);
-////should change to string??
-        String favetitle=titlefave.getText().toString();
-        //add to the database and get the new ID
-        ContentValues newfaverecipe = new ContentValues();
-        //put string name in the NAME column:
-   //     newfaverecipe.put(SaveFavoriteActivity.COL_MASSAGE,favetitle );
-
-        //insert in the database:
-    //    long newId = db.insert(SveFavouriteActivity.TABLE_NAME, null, newfaverecipe);
-        //Message messageObj = new Message(message, false,newId);
-       // objects.add(messageObj);
-      //  msgText.setText("");
-        //myAdapter.notifyDataSetChanged();//update yourself
-   // });
-
-    ///To next page  SaveFavoriteActivity
 
 }
