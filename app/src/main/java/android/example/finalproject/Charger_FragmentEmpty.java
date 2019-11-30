4dbb6fe16bb6ff7c5eae216357b8c6c297fa7a0b
catherine <zhangjiecatherine@gmail.com>
2019-11-30 10:56:47 -0500

    Second Language added

---
+++ app/src/main/java/android/example/finalproject/Charger_FragmentEmpty.java
@@ -0,0 +1,19 @@
+package android.example.finalproject;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class Charger_FragmentEmpty extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.activity_charger__fragment_empty, container, false);
+    }
+}
